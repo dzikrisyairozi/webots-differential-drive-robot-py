2524f866bb6fd2ac009c78937705a36d539d4bab
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-04-24 23:09:32 +0700

    compute linear and angular velocity

--- controllers/odometer_calculation/odometer_calculation.py
+++ controllers/odometer_calculation/odometer_calculation.py
@@ -3,6 +3,7 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+import math
 
 def run_robot(robot):
     # get the time step of the current world.
@@ -52,14 +53,27 @@ def run_robot(robot):
         
         for ind in range(2):
             diff = ps_values[ind] - last_ps_values[ind]
-            if diff < 0.001
+            if diff < 0.001:
                 diff = 0
                 ps_values[ind] = last_ps_values[ind]
             dist_values[ind] = diff * encoder_unit
             
+        #compute linear and angular velocity for robot
+        v = (dist_values[0] + dist_values[1])/2.0
+        w = (dist_values[0] - dist_values[1])/distance_between_wheels
         
+        dt = 1
+        robot_pose[2] += (w * dt)
+        
+        vx = v * math.cos(robot_pose[2])
+        vy = v * math.sin(robot_pose[2])
+        
+        robot_pose[0] += (vx * dt)
+        robot_pose[1] += (vy * dt)
+        
+        print("robot_pose: {}".format(robot_pose))
             
-        print("distance values: {} {}".format(dist_values[0], dist_values[1]))    
+        #print("distance values: {} {}".format(dist_values[0], dist_values[1]))    
         
         
         left_motor.setVelocity(max_speed)
