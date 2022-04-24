e2a9f46927654d56213507060d850fa278c56d13
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-04-24 21:57:12 +0700

    create motor instances

--- controllers/differential-drive-robot-controller/differential-drive-robot-controller.py
+++ controllers/differential-drive-robot-controller/differential-drive-robot-controller.py
@@ -4,29 +4,38 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
-# create the Robot instance.
-robot = Robot()
+if __name__ == "__main__":
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+    # create the Robot instance.
+    robot = Robot()
+    
+    # get the time step of the current world.
+    timestep = int(robot.getBasicTimeStep())
+    
+    # You should insert a getDevice-like function in order to get the
+    # instance of a device of the robot. Something like:
+    #  motor = robot.getDevice('motorname')
+    #  ds = robot.getDevice('dsname')
+    #  ds.enable(timestep)
+    
+    #Motor Instances
+    left_motor = robot.getDevice('motor_1')
+    right_motor = robot.getDevice('motor_2')
+    
+    
+    
+    # Main loop:
+    # - perform simulation steps until Webots is stopping the controller
+    while robot.step(timestep) != -1:
+        # Read the sensors:
+        # Enter here functions to read sensor data, like:
+        #  val = ds.getValue()
+    
+        # Process sensor data here.
+    
+        # Enter here functions to send actuator commands, like:
+        #  motor.setPosition(10.0)
+        pass
+    
+    # Enter here exit cleanup code.
+    
