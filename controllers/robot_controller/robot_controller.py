6f872769e36d7eb37d6ad05a8b4791d159accc29
ryohilmi <ryodm.74@gmail.com>
2022-05-01 05:35:46 +0700

    feat: add 8 compass points turning
    - remove turn function and change it to turn_to
    - add arg as the second element to state_queue
    - remove unused variables

--- controllers/robot_controller/robot_controller.py
+++ controllers/robot_controller/robot_controller.py
@@ -35,25 +35,6 @@ if __name__ == "__main__":
     right_motor.setPosition(float('inf'))
     right_motor.setVelocity(0.0)
 
-    num_side = 4
-    length_side = 0.25
-
-    wheel_radius = 0.025
-    linear_velocity = wheel_radius * max_speed
-
-    duration_side = length_side / linear_velocity
-
-    angle_of_rotation = 6.28 / num_side
-    distance_between_wheels = 0.090
-    rate_of_rotation = (2 * linear_velocity) / distance_between_wheels
-    duration_turn = angle_of_rotation / rate_of_rotation
-
-    rot_start_time = -1
-    rot_end_time = -1
-    current_time = 0
-
-    turn_side = None
-
     x, y, z = gps.getValues()
     prev_position = (x, y)
 
@@ -66,7 +47,7 @@ if __name__ == "__main__":
     prev_state = None
     initial = False
 
-    route = get_route((0, 0), (4, 5))
+    route = get_route((0, 0), (7, 7))
     current_x, current_y = route.pop(0)
     state_queue = []
 
@@ -80,29 +61,25 @@ if __name__ == "__main__":
         global orientation
         orientation = get_next_orientation(orientation) if direction == Direction.RIGHT else get_prev_orientation(orientation)
 
-    def turn(direction):
-        global rot_start_time, rot_end_time, current_time, turn_side, robot_state, orientation, ongoing_motion, target_yaw
+    def turn_to(target_orientation):
+        global robot_state, orientation, ongoing_motion, target_yaw
         ongoing_motion += 1
-        turn_side = direction
 
         PI = 3.14159265359
 
-        orientation_index = get_next_orientation(orientation).value if turn_side is Direction.RIGHT \
-                else get_prev_orientation(orientation).value
-        target_yaw = round((PI - (PI / 4 * orientation_index) + PI / 4), 2)
+        target_yaw = round((PI - (PI / 4 * target_orientation.value) + PI / 4), 2)
 
-        rot_start_time = current_time + duration_side
-        rot_end_time = rot_start_time + duration_turn
         next_state(State.TURN)
-        update_orientation(direction)
-    
-    def append_and_update(direction):
-        global state_queue
-        state_queue.append(direction)
-        if direction == 'd':
-            update_orientation(Direction.RIGHT)
-        elif direction == 'a':
-            update_orientation(Direction.LEFT)
+        orientation = target_orientation
+
+    def append_and_update(target):
+        global state_queue, target_orientation
+
+        if orientation is target:
+            return
+
+        state_queue.append(('t', target))
+        target_orientation = target
 
     def append_state():
         global state_queue, current_x, current_y
@@ -114,25 +91,32 @@ if __name__ == "__main__":
         current_x = target_node_x
         current_y = target_node_y
 
-        if (orientation == Orientation.NORTH and dy < 0) or (orientation == Orientation.SOUTH and dy > 0) \
-            or (orientation == Orientation.EAST and dx < 0) or (orientation == Orientation.WEST and dx > 0):
-            append_and_update('d')
-            append_and_update('d')
-        elif (orientation == Orientation.NORTH and dx > 0) or (orientation == Orientation.SOUTH and dx < 0) \
-            or (orientation == Orientation.EAST and dy < 0) or (orientation == Orientation.WEST and dy > 0):
-            append_and_update('d')
-        elif (orientation == Orientation.NORTH and dx < 0) or (orientation == Orientation.SOUTH and dx > 0) \
-            or (orientation == Orientation.EAST and dy > 0) or (orientation == Orientation.WEST and dy < 0):
-            append_and_update('a')
+        if (dy > 0 and dx > 0):
+            append_and_update(Orientation.NORTH_EAST)
+        elif (dy > 0 and dx < 0):
+            append_and_update(Orientation.NORTH_WEST)
+        elif (dy < 0 and dx > 0):
+            append_and_update(Orientation.SOUTH_EAST)
+        elif (dy < 0 and dx < 0):
+            append_and_update(Orientation.SOUTH_WEST)
+        elif (dy > 0):
+            append_and_update(Orientation.NORTH)
+        elif (dy < 0):
+            append_and_update(Orientation.SOUTH)
+        elif (dx > 0):
+            append_and_update(Orientation.EAST)
+        elif (dx < 0):
+            append_and_update(Orientation.WEST)
 
-        state_queue.append('w')
+        state_queue.append(('w', None))
     
     for i in range (0, len(route)):
         append_state()
     
     # print(state_queue)
-    key = state_queue.pop(0)
+    key, arg = state_queue.pop(0)
     orientation = Orientation.NORTH
+    target_orientation = Orientation.NORTH
     current_x = 0
     current_y = 0
     ongoing_motion = 0
@@ -144,12 +128,9 @@ if __name__ == "__main__":
         
         if key == 'w' and current_state == State.IDLE and ongoing_motion == 0:
             next_state(State.MOVE_FORWARD)
-            
-        if key == 'a' and current_state != State.TURN and ongoing_motion == 0:
-            turn(Direction.LEFT)
-
-        if key == 'd' and current_state != State.TURN and ongoing_motion == 0:
-            turn(Direction.RIGHT)
+        
+        if key == 't' and current_state != State.TURN and ongoing_motion == 0:
+            turn_to(arg)
 
         current_time = robot.getTime()
 
@@ -163,7 +144,7 @@ if __name__ == "__main__":
             
             if len(state_queue) <= 0:
                 break
-            key = state_queue.pop(0)
+            key, arg = state_queue.pop(0)
 
         elif current_state == State.TURN:
             yaw = imu.getRollPitchYaw()[2]
@@ -174,7 +155,7 @@ if __name__ == "__main__":
                 right_speed = 0
                 next_state(State.IDLE)
             else:
-                if turn_side == Direction.LEFT:
+                if target_yaw > abs(yaw):
                     left_speed = -0.5 * max_speed
                     right_speed = 0.5 * max_speed
                 else:
