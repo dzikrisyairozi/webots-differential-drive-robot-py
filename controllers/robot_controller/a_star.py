dd9ab6b63fd66dde3a56e41863546edfe1109bfd
ryohilmi <ryodm.74@gmail.com>
2022-05-01 06:10:46 +0700

    feat: add diagonal edge

--- controllers/robot_controller/a_star.py
+++ controllers/robot_controller/a_star.py
@@ -60,6 +60,10 @@ def create_edge():
                     addedge((i, j), (i, j+1), 1)
                 if j - 1 >= 0 and maps[i][j-1] == 0:
                     addedge((i, j), (i, j-1), 1)
+                if j + 1 < n and i + 1 < n and maps[i+1][j+1] == 0:
+                    addedge((i, j), (i+1, j+1), 1)
+                if j - 1 >= 0 and i + 1 < n and maps[i+1][j-1] == 0:
+                    addedge((i, j), (i+1, j-1), 1)
 
 def set_heuristic():
     n = len(maps)
